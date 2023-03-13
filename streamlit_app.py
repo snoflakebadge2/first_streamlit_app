47474553eab7ef822ade85a3c6f97c3acdb2d7dd
snoflakebadge2 <127756240+snoflakebadge2@users.noreply.github.com>
2023-03-13 15:34:54 +0200

    Update streamlit_app.py
    more changes to the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents new healthy dinner')
-streamlit.text('2 steaks')
-streamlit.text('1 bowl of brocolli')
-streamlit.text('10 oz Diet Coke')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
